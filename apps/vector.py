e46c743d0ea52bde6e38b0b73efd97991cf4155e
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 11:14:14 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -7,11 +7,9 @@ def app():
 
     m = leafmap.Map(center=[0, 0], zoom=2)
     m.add_basemap("HYBRID")
-    m.add_basemap("Esri.NatGeoWorldMap")
     
-    in_bounds = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_camsur_municities.geojson'
+    in_bounds = 'https://raw.githubusercontent.com/faeldon/philippines-json-maps/master/geojson/provinces/medres/provinces-region-ph050000000.0.01.json'
     m.add_geojson(in_bounds, layer_name='Camarines Sur Municities')
-    in_geojson = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/ai.geojson'
-    m.add_geojson(in_geojson, layer_name="AI")
+    
 
     m.to_streamlit(height=700)
