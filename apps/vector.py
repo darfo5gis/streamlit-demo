29a723ca43b84e06223917010cb0c920634e3335
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 12:33:10 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import leafmap.deck as leafmap
 
 
 def app():
@@ -9,6 +9,6 @@ def app():
     m= leafmap.Map(center=[13.5, 123.15], zoom=8)
     
     in_geojson = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_municities_camsur.json'
-    m.add_geojson(in_geojson, layer_name="Camarines Sur Municities Boundaries")
+    m.add_vector(in_geojson, layer_name="Camarines Sur Municities Boundaries", random_color_column="ADM_03")
     
     m.to_streamlit(height=900)
