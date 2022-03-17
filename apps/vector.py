af3b4081c32aaa175f17593c4bac34ceb06589b8
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 14:49:45 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -13,16 +13,16 @@ def app():
     municities = (
         'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_municities_camsur.json'
     )
-    config = {
-    'version': 'v1',
-    'config': {
-        'mapState': {
-            'latitude': 13.5,
-            'longitude': 123.15
-            'zoom': 8
-            }
-        }
-    }
+    #config = {
+    #'version': 'v1',
+    #'config': {
+    #    'mapState': {
+    #        'latitude': 13.5,
+    #        'longitude': 123.15
+    #        'zoom': 8
+    #        }
+    #    }
+    #}
     gdf = gpd.read_file(municities)
     m.add_gdf(gdf, layer_name='Municities', config=config)
     
