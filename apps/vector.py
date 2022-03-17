475412c848a31b687984418e7ddfa1dd3b11d6f7
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 14:32:53 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -7,13 +7,13 @@ def app():
 
     st.title("Vector")
 
-    m= leafmap.Map(center=[13.5, 123.15], zoom=8, config=config)
+    m= leafmap.Map(center=[13.5, 123.15], zoom=8)
     
     municities = (
         'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_municities_camsur.json'
     )
 
     gdf = gpd.read_file(municities)
-    m.add_gdf(gdf, layer_name='Municities')
+    m.add_gdf(gdf, layer_name='Municities', config=config)
     
     m.to_streamlit(height=900)
