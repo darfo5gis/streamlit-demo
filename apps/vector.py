c7ac228ab1871707134d13e78cf9ada33cd4608b
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 11:59:40 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+
 def app():
 
-    st.title("Vector data")
+    st.title("Vector")
 
-    m = leafmap.Map(center=[13.25, 124.15], zoom=8)
-    m.add_basemap("HYBRID")
+    m= leafmap.Map(center=[13.5, 123.15], zoom=8)
     
-    #in_bounds = 'https://raw.githubusercontent.com/faeldon/philippines-json-maps/master/geojson/provinces/medres/provinces-region-ph050000000.0.01.json'
-    #m.add_geojson(in_bounds, layer_name='Camarines Sur Municities')
+    in_geojson = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_municities_camsur.json'
+    m.add_geojson(in_geojson, layer_name="Camarines Sur Municities Boundaries")
     
-
-    m.to_streamlit(height=max)
+    m.to_streamlit(height=700)
