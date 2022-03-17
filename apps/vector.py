2c14cf28471e2239a28a83b7d0ff7eb04644f670
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 11:31:25 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -5,11 +5,11 @@ def app():
 
     st.title("Vector data")
 
-    m = leafmap.Map(center=[0, 0], zoom=2)
+    m = leafmap.Map(center=[13.25, 124.15], zoom=8)
     m.add_basemap("HYBRID")
     
-    in_bounds = 'https://raw.githubusercontent.com/faeldon/philippines-json-maps/master/geojson/provinces/medres/provinces-region-ph050000000.0.01.json'
-    m.add_geojson(in_bounds, layer_name='Camarines Sur Municities')
+    #in_bounds = 'https://raw.githubusercontent.com/faeldon/philippines-json-maps/master/geojson/provinces/medres/provinces-region-ph050000000.0.01.json'
+    #m.add_geojson(in_bounds, layer_name='Camarines Sur Municities')
     
 
     m.to_streamlit(height=700)
