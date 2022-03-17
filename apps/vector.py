2a39b6e4b5453768490af409afeb2cffb12f8eb2
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 10:20:10 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -6,8 +6,13 @@ def app():
     st.title("Vector data")
 
     m = leafmap.Map(center=[0, 0], zoom=2)
-
-    in_geojson = 'https://'
-    m.add_geojson(in_geojson, layer_name="")
+    m.add_basemap("HYBRID")
+    m.add_basemap("Esri.NatGeoWorldMap")
+    m.whiteboxgui(tree=True)
+    
+    in_bounds = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_camsur_municities.geojson'
+    m.add_geojson(in_bounds, layer_name='Camarines Sur Municities')
+    in_geojson = 'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/ai.geojson'
+    m.add_geojson(in_geojson, layer_name="AI")
 
     m.to_streamlit(height=700)
