951defcc1aa99228e4fd4a965c6110d21e06420a
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-17 14:49:01 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -2,16 +2,8 @@ import streamlit as st
 import leafmap.kepler as leafmap
 import geopandas as gpd
 
-config = {
-    'version': 'v1',
-    'config': {
-        'mapState': {
-            'latitude': 13.5,
-            'longitude': 123.15
-            'zoom': 8
-        }
-    }
-}
+
+
 def app():
 
     st.title("Vector")
@@ -21,7 +13,16 @@ def app():
     municities = (
         'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/r5_municities_camsur.json'
     )
-
+    config = {
+    'version': 'v1',
+    'config': {
+        'mapState': {
+            'latitude': 13.5,
+            'longitude': 123.15
+            'zoom': 8
+            }
+        }
+    }
     gdf = gpd.read_file(municities)
     m.add_gdf(gdf, layer_name='Municities', config=config)
     
