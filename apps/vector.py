6ed26217189e03ea5598ef14a75ca81122b60681
darfo5gis <101569531+darfo5gis@users.noreply.github.com>
2022-03-18 13:44:27 +0800

    Update vector.py

--- apps/vector.py
+++ apps/vector.py
@@ -19,6 +19,9 @@ def app():
     bp2_benes = (
         'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/data/vector/bp2benes.geojson'
     )
+    config= = (
+        'https://raw.githubusercontent.com/darfo5gis/streamlit-demo/master/config/vector/m_config.json'
+    )
 
     gdf = gpd.read_file(municities)
     gdf1 = gpd.read_file(ai)
@@ -26,5 +29,5 @@ def app():
     m.add_gdf(gdf, layer_name='Municities')
     m.add_gdf(gdf1, layer_name='AI case')
     m.add_gdf(gdf2, layer_name='BP2 Livelihood Assistance beneficiaries')
-    
+    m.load_config()
     m.to_streamlit(height=900, width=900, responsive=True, scrolling=True)
